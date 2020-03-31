27339eb77c240867917d007f1bbc3b50d86954f2
Semenu Aggor <semenu.aggor@solirius.com>
2020-03-31 13:07:55 +0100

    ALS-361 Test that correct exit code is returned

--- stack-plan.sh
+++ stack-plan.sh
@@ -1,20 +1,27 @@
 #!/usr/bin/env bash
-set +e
+set -e
 
 source ${CODEBUILD_SRC_DIR}/env_configs/${environment_name}/${environment_name}.properties
 
 env | sort
 
+function init_plan() {
+    if [ -d .terraform ]; then
+        rm -rf .terraform
+    fi
+    sleep 1
+    terragrunt init
+    terragrunt plan -detailed-exitcode --out ${environment_name}.plan > tf.plan.out
+    exitcode="$?"
+    if [ "$exitcode" -ne 0 ]; then
+        exit "$exitcode"
+    fi
+
+    cat tf.plan.out
+
+    parse-terraform-plan -i tf.plan.out | jq '.changedResources[] | (.action != "update") or (.changedAttributes | to_entries | map(.key != "tags.source-hash") | reduce .[] as $item (false; . or $item))' | jq -e -s 'reduce .[] as $item (false; . or $item) == false'
+}
+
 for d in ./*/ ; do
-    (cd "$d" && if [ -d .terraform ]; then rm -rf .terraform; fi; sleep 1; terragrunt init; terragrunt plan -detailed-exitcode --out ${environment_name}.plan > tf.plan.out; exitcode="\$?"; if [ "\$exitcode" -ne 0 ]; then exit "\$exitcode"; fi;)
+    (cd "$d" && init_plan)
 done
-
-cat tf.plan.out
-
-if [ exitcode == '1' ]; then
-    exit 1
-fi
-
-parse-terraform-plan -i tf.plan.out | jq '.changedResources[] | (.action != "update") or (.changedAttributes | to_entries | map(.key != "tags.source-hash") | reduce .[] as $item (false; . or $item))' | jq -e -s 'reduce .[] as $item (false; . or $item) == false'
-
-set -e
