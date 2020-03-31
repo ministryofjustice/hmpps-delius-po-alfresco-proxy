4da37862f0bcd10ab6f6ee8a98f2559dcf8eeb44
Semenu Aggor <semenu.aggor@solirius.com>
2020-03-31 10:11:14 +0100

    ALS-361 create test results s3 bucket - update pipeline to create terraform resources - refactor buildspec to remove amazon jdk

---
+++ src/main/terraform/s3-bucket-test-results/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
+
+provider "aws" {
+  region  = "${var.region}"
+  version = ">= 2.55.0"
+}
+
+############################################
+# S3 bucket
+############################################
+
+# #-------------------------------------------
+# ### S3 bucket for ci build test results
+# #-------------------------------------------
+
+module "s3_spg_ci_build_test_results_bucket" {
+  source         = "git::https://github.com/ministryofjustice/hmpps-terraform-modules.git?ref=master//modules//s3bucket//s3bucket_without_policy"
+  s3_bucket_name = "${var.environment_identifier}-${var.bucket_identifier}"
+  tags           = "${var.tags}"
+}
