6c0a23f3a99ba43205365fa373fabdbd318eeca1
Semenu Aggor <semenu.aggor@solirius.com>
2020-03-31 12:12:09 +0100

    ALS-361 Test injecting env configs path

--- stack-plan.sh
+++ stack-plan.sh
@@ -3,10 +3,7 @@ set +e
 
 env | sort
 
-rm -rf src/main/terraform/common_env_configs
-git clone https://github.com/ministryofjustice/hmpps-env-configs.git src/main/terraform/common_env_configs
-
-source src/main/terraform/common_env_configs/${environment_name}/${environment_name}.properties
+source env_configs/${environment_name}/${environment_name}.properties
 
 for d in ./*/ ; do
     (cd "$d" && if [ -d .terraform ]; then rm -rf .terraform; fi; sleep 1; terragrunt init; terragrunt plan -detailed-exitcode --out ${environment_name}.plan > tf.plan.out; exitcode="\$?"; if [ "\$exitcode" -ne 0 ]; then exit "\$exitcode"; fi;)
