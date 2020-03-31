ec9979c04ad8225e7fa6ce7ab1eef53b601f17a5
Semenu Aggor <semenu.aggor@solirius.com>
2020-03-31 12:21:29 +0100

    ALS-361 Test injecting env configs path

--- stack-plan.sh
+++ stack-plan.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set +e
 
-env | sort
+source ${CODEBUILD_SRC_DIR}/env_configs/${environment_name}/${environment_name}.properties
 
-source env_configs/${environment_name}/${environment_name}.properties
+env | sort
 
 for d in ./*/ ; do
     (cd "$d" && if [ -d .terraform ]; then rm -rf .terraform; fi; sleep 1; terragrunt init; terragrunt plan -detailed-exitcode --out ${environment_name}.plan > tf.plan.out; exitcode="\$?"; if [ "\$exitcode" -ne 0 ]; then exit "\$exitcode"; fi;)
