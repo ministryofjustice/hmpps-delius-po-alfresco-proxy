a38a691946e53c53bc6c46c2a97f875a64f66cdd
Semenu Aggor <semenu.aggor@solirius.com>
2020-04-27 14:21:48 +0100

    ALS-457 Test describe-tasks command

--- scripts/describe-tasks.sh
+++ scripts/describe-tasks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-taskArns=`aws ecs list-tasks --cluster $cluster_name | jq '.taskArns'`
-aws ecs describe-tasks --cluster $cluster_name --tasks $taskArns
+taskArns=`aws ecs list-tasks --cluster $cluster_arn | jq '.taskArns'`
+aws ecs describe-tasks --cluster $cluster_arn --tasks $taskArns
