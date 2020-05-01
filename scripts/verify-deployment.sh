db062d90d9d26ec4fd9311e11c2a83cc9abad92b
Semenu Aggor <semenu.aggor@solirius.com>
2020-05-01 15:55:25 +0100

    ALS-457 Improve logic for deployment verification - allow 10 minutes for deployment to complete or time out - check that the task is running

--- scripts/verify-deployment.sh
+++ scripts/verify-deployment.sh
@@ -8,17 +8,20 @@ preDeploymentVersionStr=`cat ${HMPPS_BUILD_WORK_DIR}/${pre_deployment_taskdef_fi
 preDeploymentVersion="$(($preDeploymentVersionStr + 0))"
 currentVersion=${preDeploymentVersion}
 
-NEXT_WAIT_TIME=0
-TIME_OUT=20
+next_wait_time=0
 counter=0
-until [ ${currentVersion} -gt ${preDeploymentVersion} ] || [ $NEXT_WAIT_TIME -eq ${TIME_OUT} ]; do
+lastStatus=""
+desiredStatus=""
+until ([ ${currentVersion} -gt ${preDeploymentVersion} ] && [ ${lastStatus} == "RUNNING" ] && [ ${desiredStatus} == "RUNNING" ]) || [ $next_wait_time -eq ${DEPLOY_WAIT_TIME_OUT} ]; do
     let counter=counter+1
     scripts/describe-tasks.sh ${task_group} ${post_deployment_taskdef_file}
-    echo "--------------- counter=$counter ---------------"
+    echo "--------------- counter=${counter} ---------------"
     cat ${HMPPS_BUILD_WORK_DIR}/${post_deployment_taskdef_file}
     echo "------------------------------------------------"
     tempVer=`cat ${HMPPS_BUILD_WORK_DIR}/${post_deployment_taskdef_file} | jq '.taskDefVersion' | awk '{print substr($1,2,length($1)-2)}'`
-    currentVersion=$(($tempVer + 0))
-    sleep 5
-    let NEXT_WAIT_TIME=NEXT_WAIT_TIME+5
+    let currentVersion=$(($tempVer + 0))
+    let lastStatus=`cat ${HMPPS_BUILD_WORK_DIR}/${post_deployment_taskdef_file} | jq '.lastStatus'`
+    let desiredStatus=`cat ${HMPPS_BUILD_WORK_DIR}/${post_deployment_taskdef_file} | jq '.desiredStatus'`
+    sleep 30
+    let next_wait_time=next_wait_time+5
 done
