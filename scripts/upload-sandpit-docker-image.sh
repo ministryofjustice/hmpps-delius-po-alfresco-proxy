265a5bf2d36fc9f66a3984de74af31da02f5753c
Semenu Aggor <semenu.aggor@solirius.com>
2020-06-08 09:16:29 +0100

    NOJIRA - Fix bug in local ecs deployment - fix error in engineering role variable name

--- scripts/upload-sandpit-docker-image.sh
+++ scripts/upload-sandpit-docker-image.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ENGINEERING_TERRAFORM_IAM_ROLE="arn:aws:iam::895523100917:role/terraform"
+ENGINEERING_TERRAFORM_IAM_ROLE_ARN="arn:aws:iam::895523100917:role/terraform"
 source $(pwd)/scripts/assume-role.sh ${ENGINEERING_TERRAFORM_IAM_ROLE_ARN}
 
 eval $(aws --region ${AWS_REGION} ecr get-login --no-include-email)
