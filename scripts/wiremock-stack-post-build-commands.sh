73ed406dd61df96352e5d9e77167ef33f9a9e714
Semenu Aggor <semenu.aggor@solirius.com>
2020-06-26 09:15:06 +0100

    ALS-1040 Test curl wiremock from CI pipeline

--- scripts/wiremock-stack-post-build-commands.sh
+++ scripts/wiremock-stack-post-build-commands.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Running Wiremock stack post build stage"
-docker run --rm curlimages/curl http://alfresco.dev.delius-core.probation.hmpps.dsd.io:8080/__admin/mappings
+apk --no-cache add curl
+sleep 20
+curl http://alfresco.dev.delius-core.probation.hmpps.dsd.io:8080/__admin/mappings
