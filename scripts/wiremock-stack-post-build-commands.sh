82d7998aa6c9702b0b2f4ed75c8261e5564cfd18
Semenu Aggor <semenu.aggor@solirius.com>
2020-07-03 15:51:34 +0100

    ALS-1040 correct error in wiremock dns

--- scripts/wiremock-stack-post-build-commands.sh
+++ scripts/wiremock-stack-post-build-commands.sh
@@ -3,6 +3,8 @@
 echo "Running Wiremock stack post build stage"
 apk --no-cache add curl
 # Wait for wiremock to deploy
-echo "Waiting for wiremock to start up"
+date
+echo "Waiting for wiremock to start up (12 minutes)"
 sleep 720
-curl -sS http://wiremock-dlc-dev-spgw-alfproxy.sandpit.delius-core.probation.hmpps.dsd.io:8080/__admin/mappings
+date
+curl -sS http://wiremock-dlc-dev-spgw-alfproxy.dev.delius-core.probation.hmpps.dsd.io:8080/__admin/mappings
