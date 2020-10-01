27f1feaad9c69d019c01089e25201beef8623526
Paul Crimes <paul.crimes@solirius.com>
2020-10-01 12:54:23 +0100

    fix environment condition check

--- scripts/delete-undeployed-images.sh
+++ scripts/delete-undeployed-images.sh
@@ -13,13 +13,13 @@ echo "----------------------------------------------------------"
 echo Cleaning up undeployed images...
 
 
-if [${my_aws_env} == "sandpit"]; then
+if ["${my_aws_env}" == "sandpit"]; then
     aws ecr list-images --repository-name ${IMAGE_NAME} | \
     jq --argjson MY_IMAGE_TAG ${image_tag} -r '.imageIds[] | select(.imageTag | startswith("sandpit")) | select(.imageTag != "sandpit-2-") | select(.imageTag != $MY_IMAGE_TAG) | [.imageDigest] | @tsv' | \
     while IFS=$'\t' read -r digest; do
         aws ecr batch-delete-image --repository-name ${IMAGE_NAME} --image-ids imageDigest=${digest}
     done
-elif [${my_aws_env} == "sandpit-2"]; then
+elif ["${my_aws_env}" == "sandpit-2"]; then
     aws ecr list-images --repository-name ${IMAGE_NAME} | \
     jq --argjson MY_IMAGE_TAG ${image_tag} -r '.imageIds[] | select(.imageTag | startswith("sandpit-2-")) | select(.imageTag!=$MY_IMAGE_TAG) | [.imageDigest] | @tsv' | \
     while IFS=$'\t' read -r digest; do
